6cb979f013678d434596cab622f2c8e3c604a3a4
FranciscoCardenas147 <135192587+FranciscoCardenas147@users.noreply.github.com>
2023-06-02 23:56:55 -0400

    Version 1.2
    Version 1.2

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+
+
+function validacion(){
+  if (!ganancia||!monto ){
+    alert("No dejes casillas en blanco");
+    return
+  }
+}
+
+
 function calcularTotal(){
     let monto = parseFloat(document.getElementById("monto").value);
     let ganancia = parseFloat(document.getElementById("ganancia").value);
@@ -5,10 +16,15 @@ function calcularTotal(){
     // let ganancia = 25;
     let totalCompraProv = monto + (monto * (impuesto / 100)); 
     let totalVenta = totalCompraProv +(totalCompraProv * (ganancia /100));
-
-    let totalFinalVenta = totalVenta + (totalVenta * (impuesto /100));
     
-    document.getElementById("resultado").innerHTML = 'Este es el precio de Venta al  ' + ganancia + '% de ganancia = $ '+'' + totalFinalVenta.toFixed(1);
+    let totalFinalVenta = totalVenta + (totalVenta * (impuesto /100));
+
+    if (!ganancia||!monto ){
+      alert("No dejes casillas en blanco");
+      return
+    }
+
+    document.getElementById("resultado").innerHTML = 'Precio de Venta al  ' + ganancia + '% de ganancia = $ '+'' + totalFinalVenta.toFixed(1);
 }
 
 
@@ -21,7 +37,12 @@ function calcularTotal25(){
 
     let totalFinalVenta = totalVenta + (totalVenta * (impuesto /100));
     
-    document.getElementById("resultado25").innerHTML = "Este es el precio de Venta al 25% -> $" + totalFinalVenta.toFixed(1);
+    if (!ganancia||!monto ){
+      alert("No dejes casillas en blanco");
+      return
+    }
+
+    document.getElementById("resultado25").innerHTML = "Precio de Venta al 25% -> $" + totalFinalVenta.toFixed(1);
 }
 
 function calcularTotal50() {
@@ -31,7 +52,13 @@ function calcularTotal50() {
     let totalCompraProv = monto + (monto * (impuesto / 100));  
     let totalVenta = totalCompraProv +(totalCompraProv * (ganancia /100));
     let totalFinalVenta = totalVenta + (totalVenta * (impuesto /100));
-   document.getElementById("resultado50").innerHTML = "Este es el precio a Venta al 50% -> $" + totalFinalVenta.toFixed(1);
+
+    if (!ganancia||!monto){
+      alert("No dejes casillas en blanco");
+      return
+    }
+
+   document.getElementById("resultado50").innerHTML = "Precio a Venta al 50% -> $" + totalFinalVenta.toFixed(1);
   }
 
   function calcularTotal75() {
@@ -41,8 +68,21 @@ function calcularTotal50() {
     let totalCompraProv = monto + (monto * (impuesto / 100));  
     let totalVenta = totalCompraProv +(totalCompraProv * (ganancia /100));
     let totalFinalVenta = totalVenta + (totalVenta * (impuesto /100));
-   document.getElementById("resultado75").innerHTML = "Este es el precio a Venta al 75% -> $" + totalFinalVenta.toFixed(1);
+
+    if (!ganancia||!monto ){
+      alert("No dejes casillas en blanco");
+      return
+    }
+
+   document.getElementById("resultado75").innerHTML = "Precio a Venta al 75% -> $" + totalFinalVenta.toFixed(1);
   }  
+
+function limpiar(){
+ location.reload();
+}
+
+
+
 
 
 //   aca imprimimos con enter
@@ -52,7 +92,7 @@ function calcularTotal50() {
       calcularTotal25()
       calcularTotal50()
       calcularTotal75()
-
-
     }
   });
+
+
